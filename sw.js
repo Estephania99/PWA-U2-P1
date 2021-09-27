5721c0a3082ee127f7b715f082c564d8ae41c19f
Elina <20183ti015@utez.edu.mx>
2021-09-27 11:57:04 -0500

    Prueba url images

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 //console.log('WS: Hola mundo :) ya es tarde ');
-
-
+var _window = this || self || window;
+let url = _window.location.href;
 
 self.addEventListener('install',(event)=>{
     console.log('SW: Instalado');
@@ -13,10 +13,18 @@ self.addEventListener('activate',(event)=>{
 self.addEventListener('fetch',(event)=>{
     console.log(event.request.url);
     if(event.request.url.includes('.jpg')){
-        let newResp = fetch('/images/gatito.jpg');
+        let newResp = "";
+        if(url.includes('localhost')){
+            newResp = fetch('/images/gatito.jpg');
+        }else{
+            newResp = fetch('/PWA-U2-P1/images/gatito.jpg');
+        }
+        
         console.log('Es una imagen');
         event.respondWith(newResp);
     }
+
+    
 
     if(event.request.url.includes('page.css')){
         let newResponse = new Response(`body{
